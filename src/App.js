4dedf8002450f91ffa30af6999d3210f5b0d19cc
Li Xuanji <xuanji@gmail.com>
2017-05-07 18:51:50 +0800

    use only tick, make green, add questions

--- src/App.js
+++ src/App.js
@@ -12,8 +12,16 @@ import listen from './listen.png';
 import ReactOutsideEvent from 'react-outside-event';
 
 import Q1 from './Q1.mp3';
+import Q2 from './Q2.mp3';
+import Q3 from './Q3.mp3';
+import Q4 from './Q4.mp3';
+import Q5 from './Q5.mp3';
 
 import R1 from './R1.mp3';
+import R2 from './R2.mp3';
+import R3 from './R3.mp3';
+import R4 from './R4.mp3';
+import R5 from './R5.mp3';
 
 import './App.css';
 
@@ -92,10 +100,15 @@ class _VocabEntry extends Component {
     }
   }
   renderIcon() {
-      return <img alt="rec" src={this.state.active ? recActive : rec} style={{height: '1.1em'}} />
+      return <img
+        alt="rec"
+        src={this.state.active ? recActive : rec} style={{height: '1.1em', marginRight: 5}} />
+  }
+  isCorrect() {
+    return canonicalSentence(this.state.interim_transcript + this.state.final_transcript) === canonicalSentence(this.props.rightAnswer);
   }
   renderCorrectOrWrong() {
-    if (canonicalSentence(this.state.interim_transcript + this.state.final_transcript) === canonicalSentence(this.props.rightAnswer)) {
+    if (1) {
       return <span style={{color: 'green'}}>✔</span>
     } else {
       return <span style={{color: 'red'}}>✗</span>
@@ -130,20 +143,27 @@ class _VocabEntry extends Component {
       <div style={{width: '6em'}}>
       {this.props.label}
       </div>
-      <div id="1-rec" style={{
+      <div style={{
         backgroundColor: 'white',
         height: '1.5em',
         display: 'flex',
         alignItems: 'center',
-        width: '34em'
+        width: '34em',
       }}
       onClick={this.handleClickRec.bind(this)}
       >
       {this.renderIcon()}
-      <span style={{color: 'grey'}}>{this.state.interim_transcript}</span>
-      <span>{this.state.final_transcript}</span>
+      <span style={{
+        color: this.isCorrect() ? 'green' : 'grey'
+      }}>{this.state.interim_transcript}</span>
+      <span style={{
+        color: this.isCorrect() ? 'green' : 'black' }}
+      >{this.state.final_transcript}</span>
+      {this.isCorrect()
+        ? <span style={{color: 'green', marginLeft: 'auto'}}>✔</span>
+        : null
+      }
       </div>
-      {this.renderCorrectOrWrong()}
       <img alt="restart" onClick={this.handleClickRelaunch.bind(this)} style={{cursor: 'pointer'}} src={relaunch} height={22} />
       <img alt="listen" onClick={this.handleClickListen.bind(this)} style={{cursor: 'pointer'}} src={listen} height={22} />
       </div>
@@ -196,6 +216,10 @@ class App extends Component {
       }}>
 
       <VocabEntry audioSrc={Q1} responseAudioSrc={R1} label="Question 1" rightAnswer="à paris il pleut" />
+      <VocabEntry audioSrc={Q2} responseAudioSrc={R2} label="Question 2" rightAnswer="à lyon il neige" />
+      <VocabEntry audioSrc={Q3} responseAudioSrc={R3} label="Question 3" rightAnswer="à nice il fait du soleil" />
+      <VocabEntry audioSrc={Q4} responseAudioSrc={R4} label="Question 4" rightAnswer="à rennes il y a du brouillard" />
+      <VocabEntry audioSrc={Q5} responseAudioSrc={R5} label="Question 5" rightAnswer="à strasbourg le ciel est couvert" />
 
       </div>
 
